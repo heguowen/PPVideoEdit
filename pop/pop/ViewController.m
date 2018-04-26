140d25e605b6252b6084037910ecce13cf2370ae
helinyu <2319979647@qq.com>
2018-04-26 20:38:49 +0800

    Deleting the annotated code

--- pop/pop/ViewController.m
+++ pop/pop/ViewController.m
@@ -19,18 +19,13 @@
     TZImagePickerController *_picker;
 }
 
-
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
-
-
-
 
 - (IBAction)onImportClick:(id)sender {
 
@@ -65,42 +60,7 @@
 #endif
 }
 
-
-
 #pragma mark - TSAssetsPickerControllerDataSource
-//- (NSUInteger)numberOfItemsToSelectInAssetsPickerController:(TSAssetsPickerController *)picker {
-//    return 1;
-//}
-//
-//- (TSFilter *)filterOfAssetsPickerController:(TSAssetsPickerController *)picker {
-//    return [TSFilter filterWithType:FilterTypeVideo];
-//}
-//
-//#pragma mark - TSAssetsPickerControllerDelegate
-//- (void)assetsPickerControllerDidCancel:(TSAssetsPickerController *)picker {
-//    [_picker dismissViewControllerAnimated:YES completion:nil];
-//}
-//
-//- (void)assetsPickerController:(TSAssetsPickerController *)picker didFinishPickingAssets:(NSArray<ALAsset *> *)assets {
-//    double delayInSeconds = 2.0;
-//    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-//    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-//        [_picker dismissViewControllerAnimated:YES completion:^{
-//            PPVideoEditController *editController = (PPVideoEditController *)[UIStoryboard loadControllerWithStoryBoardName:@"PPVideoEdit" withController:[PPVideoEditController class]];
-//            [editController loadVideoWithAlAsset:[assets firstObject]];
-//            [self presentViewController:editController animated:YES completion:^{
-//                
-//            }];
-//        }];
-//    });
-//}
-//
-//- (void)assetsPickerController:(TSAssetsPickerController *)picker failedWithError:(NSError *)error {
-//    if (error) {
-//        NSLog(@"Error occurs. Show dialog or something. Probably because user blocked access to Camera Roll.");
-//    }
-//}
-
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
@@ -108,15 +68,6 @@
     NSURL *url = [info objectForKey:UIImagePickerControllerMediaURL];
     double delayInSeconds = 2.0;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-//        [self dismissViewControllerAnimated:YES completion:^{
-//            PPVideoEditController *editController = (PPVideoEditController *)[UIStoryboard loadControllerWithStoryBoardName:@"PPVideoEdit" withController:[PPVideoEditController class]];
-//            [editController loadVideoWithUrl:url];
-//            [self presentViewController:editController animated:YES completion:^{
-//                
-//            }];
-//        }];
-    });
 }
 
 
